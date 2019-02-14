adc699fe87bae3a879a72c9501c8cd006870d6f9
Matthew Reid <merknicks11@gmail.com>
2019-02-14 12:45:04 -0500

    almost done with part A

--- Vector.cpp
+++ Vector.cpp
@@ -1,9 +1,32 @@
 #include "Vector.h"
 
 Vector::Vector(int size){
-	this->array =
+	this->array = new Planet[size];
 }
 
 Planet* Vector::read(int index) {
+	if (index >= this->size() || index < 0) return NULL;
+	return this->array[index];
+}
 
+Vector::~Vector() {
+	int max = size();
+	for (int i=0; i<max; i++) delete this->array[i];
+}
+
+void Vector::insert(int index, Planet * p) {
+	if (index < this->size()) this->array[index] = p;
+	else {
+		// increase size to index+1
+		this->array[index] = p;
+	}
+}
+
+bool Vector::remove(int index) {
+	if (index < this->size() && index >= 0) {
+	this->array[index] = NULL;
+	// decrease array size
+	return true;
+	}
+	return false;
 }
