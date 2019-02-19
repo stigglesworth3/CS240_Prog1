f2d2e59102b1d9f1946996bd67af44e428f04301
Kyle Stigler <38743395+stigglesworth3@users.noreply.github.com>
2019-02-18 20:56:57 -0500

    Add files via upload

---
+++ List.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cstdlib>
+#include "Planet.h"
+#include "Star.h"
+
+List::List()
+{
+	this->head = NULL;
+	this->tail = NULL;
+}
+
+List::~List()
+{
+	for (int j=0; j<this->size(); j++)
+	{
+		this->head = this->head->next;
+		delete this->head->prev;
+	}
+}
+
+void List::insert(int index, Planet * p)
+{
+	if (this->head == NULL)
+	{
+		this->head = p;
+		this->tail = p;
+		this->numNodes++;
+	}
+	else
+	{
+		if (index >= this->size())
+		{
+			this->tail->next = p;
+			p->prev = this->tail;
+			this->tail = p;
+			this->numNodes++;
+		}
+		else
+		{
+			if (index == 0)
+			{
+				this->head->prev = p;
+				p->next = this->head;
+				this->head = p;
+				this->numNodes++;
+			}
+			else
+			{
+				Planet * in = this->head->next;
+				for (int i=1; i<this->size(); i++)
+				{
+					if (i == index)
+					{
+						in->prev->next = p;
+						p->next = in;
+						in->prev = p;
+						this->numNodes++;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+Planet * List::read(int index)
+{
+	if (index < 0 || index >= this->size())
+	{
+		return NULL;
+	}
+	PLanet * ret = this->head;
+	for (int i=0; i<index; i++)
+	{
+		ret = ret->next;
+	}
+	return ret;
+}
+
+bool List::remove(int index)
+{
+	if (index < 0 || index >=size())
+	{
+		return false;
+	}
+	Planet * gone = this->read(index);
+	if (gone == NULL)
+	{
+		return false;
+	}
+	gone->prev->next = gone->next;
+	gone->next->prev = gone->prev;
+	this->numNodes--;
+	return true;
+}
+
+unsigned List::size()
+{
+	return this->numNodes;
+}
---
+++ List.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+#include <cstdlib>
+#include "Planet.h"
+#include "Star.h"
+
+class List()
+{
+	Node * head;
+	Node * tail;
+	unsigned numNodes;
+	~List();
+	void insert(int, Planet*);
+	Planet * read(int);
+	bool remove(int);
+	unsigned size();
+	
+};
+
+class Node()
+{
+	Planet * plan;
+	Node * next;
+	Node * prev;
+};
+#endif
