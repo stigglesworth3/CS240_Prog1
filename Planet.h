3fec7ca36ce6acd93deb7b20ca6ccca962fdeca0
Kyle Stigler <kstigle1@binghamton.edu>
2019-02-21 12:12:27 -0500

    planet.h/cpp

--- Planet.h
+++ Planet.h
@@ -9,13 +9,13 @@ class Planet {
 
 	public:
 		Planet(){this->id=-1;};
+		Planet(int);
+		Planet(int, int);
 
-		Planet(int,int);
-
-    int orbit();
-		long Planet::getID() { return id; }
-		int Planet::getDistance() { return distance; }
-		int Planet::getPos() { return pos; }
-		char Planet::getType() { return type; }
+    		int orbit();
+		long getID() { return id; }
+		int getDistance() { return distance; }
+		int getPos() { return pos; }
+		char getType() { return type; }
 		//you may add any additional methods you may need.
 };
