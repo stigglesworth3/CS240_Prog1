257a0a25bf9adf693e6e80e085e1916a2f3255ce
Matthew Reid <merknicks11@gmail.com>
2019-03-05 18:22:07 -0500

    list

--- List.cpp
+++ List.cpp
@@ -5,17 +5,17 @@
 
 List::List()
 {
-	this->head = NULL;
-	this->tail = NULL;
+    this->head = NULL;
+    this->tail = NULL;
 }
 
 List::~List()
 {
-	for (int j=0; j<this->size(); j++)
-	{
-		this->head = this->head->next;
-		delete this->head->prev;
-	}
+    for (int j=0; j<this->size(); j++)
+    {
+        this->head = this->head->next;
+        delete this->head->prev;
+    }
 }
 
 void List::insert(long index, Planet * p)
@@ -25,39 +25,39 @@ void List::insert(long index, Planet * p)
 
 Planet * List::read(int index)
 {
-	if (index < 0 || index >= this->size())
-	{
-		return NULL;
-	}
-	Node * ret = this->head;
-	for (int i=0; i<index; i++)
-	{
-		ret = ret->next;
-	}
-	return ret->plan;
+    if (index < 0 || index >= this->size())
+    {
+        return NULL;
+    }
+    Node * ret = this->head;
+    for (int i=0; i<index; i++)
+    {
+        ret = ret->next;
+    }
+    return ret->plan;
 }
 
 bool List::remove(int index)
 {
-	if (index < 0 || index >=size())
-	{
-		return false;
-	}
-	Node * gone;
-	gone->plan = this->read(index);
-	if (gone->plan == NULL)
-	{
-		return false;
-	}
-	gone->prev->next = gone->next;
-	gone->next->prev = gone->prev;
-	delete gone->plan;
-	gone->plan = NULL;
-	this->numNodes--;
-	return true;
+    if (index < 0 || index >=size())
+    {
+        return false;
+    }
+    Node * gone;
+    gone->plan = this->read(index);
+    if (gone->plan == NULL)
+    {
+        return false;
+    }
+    gone->prev->next = gone->next;
+    gone->next->prev = gone->prev;
+    delete gone->plan;
+    gone->plan = NULL;
+    this->numNodes--;
+    return true;
 }
 
 unsigned List::size()
 {
-	return this->numNodes;
+    return this->numNodes;
 }
